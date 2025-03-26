56a005a74d108b591ed153062a817addb6312f5c
gabipcx <gabrielapinhati79@gmail.com>
2025-03-26 10:45:10 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './css/style.css'
+
+function App() {
+    return (
+      <div className="App">
+        <main>
+          <section id="about">
+            <h1>GABRIELA!</h1>
+            <img src='https://pbs.twimg.com/media/FC9Ppm9VUAoDwXi.jpg'/>
+            <h2>Oii, eu me chamo gabriela tenho 17 anos e gosto de pintura, desenho, brecho e minhas cachorras</h2>
+          </section>
+        </main>
+      </div>
+    );
+  }
+  
+  
+  export default App;
